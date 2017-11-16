83e9fb2b6ecd08c90d2f92b7ed1f925c27c88c10
rabiulislam993 <rb60041@gmail.com>
2017-11-16 23:39:12 +0600

    basic api added

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url, include
 
 # from rest_framework.documentation import include_docs_urls
-from .views import APIDocAPIView
+from .views import APIDoc
 
 
 urlpatterns = [
-    url(r'^$', APIDocAPIView.as_view(), name='api_doc'),
+    url(r'^$', APIDoc.as_view(), name='api_doc'),
     url(r'^result/', include('result.api.urls', namespace='api')),
     # url(r'^', include_docs_urls(title='Result Publishing API', description='RESTful API for Result')),
 ]
--- api/views.py
+++ api/views.py
@@ -1,28 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
 # Create your views here.
-class APIDocAPIView(APIView):
-    def get(self, request):
-        doc_dict = {
-            'API URL':'www.befaq.ml/api/result/  for single result, '
-                      'www.befaq.ml/api/result/top/ for top results list,',
+# class APIDocAPIView(APIView):
+#     def get(self, request):
+#         doc_dict = {
+#             'API URL':'www.befaq.ml/api/result/  for single result, '
+#                       'www.befaq.ml/api/result/top/ for top results list,',
+#
+#             'retrieve single result from get request': 'to retrieve a student result make a get request to '
+#                                                        'www.befaq.ml/api/result/ with tow arguments '
+#                                                        '1) reg_id , send student registration ID in reg_id argument '
+#                                                        '2) reg_year, send student registration Year in reg_year argument',
+#
+#             'retrieve single result from flat URL': 'to retrieve a student result make a get request to '
+#                                                     'www.befaq.ml/api/result/<reg_year>/<reg_id>/ where reg_year is '
+#                                                     'student registration year and reg_id is student registration ID '
+#                                                     'example: go to www.befaq.ml/api/result/2017/2/ for retrieve result of '
+#                                                     'student whose registration ID is 2 and registration year is 2017',
+#
+#             'retrieve top results list': 'top result',
+#         }
+#
+#         return Response(doc_dict)
 
-            'retrieve single result from get request': 'to retrieve a student result make a get request to '
-                                                       'www.befaq.ml/api/result/ with tow arguments '
-                                                       '1) reg_id , send student registration ID in reg_id argument '
-                                                       '2) reg_year, send student registration Year in reg_year argument',
-
-            'retrieve single result from flat URL': 'to retrieve a student result make a get request to '
-                                                    'www.befaq.ml/api/result/<reg_year>/<reg_id>/ where reg_year is '
-                                                    'student registration year and reg_id is student registration ID '
-                                                    'example: go to www.befaq.ml/api/result/2017/2/ for retrieve result of '
-                                                    'student whose registration ID is 2 and registration year is 2017',
-
-            'retrieve top results list': 'top result',
-        }
-
-        return Response(doc_dict)
+class APIDoc(TemplateView):
+    template_name = 'api/api_doc.html'
