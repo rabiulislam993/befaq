849e5213b74efe9c6f5721bf011bbd587b0957ac
rabiulislam993 <rb60041@gmail.com>
2017-11-23 12:16:00 +0600

    api doc flatpage added

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^result/', include('result.api.urls', namespace='result_api')),
-    url(r'^$', TemplateView.as_view(template_name='api/api_doc.html'), name='api_doc'),
+    # url(r'^$', TemplateView.as_view(template_name='api/api_doc.html'), name='api_doc'),
 ]
